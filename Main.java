1f74b7a3d19450ee876b4a19724d5f9148395c81
HacheD25 <miguelcaeirogarcia92@gmail.com>
2025-03-21 10:39:19 +0100

    Refactorización main.java

--- Main.java
+++ Main.java
@@ -1,21 +1,38 @@
+package cuentas;
+
+/**
+ * La clase Main ejecuta la operativa de la cuenta bancaria.
+ */
 public class Main {
 
+    /**
+     * Método principal que invoca el método operativa_cuenta.
+     * @param args Argumentos de línea de comandos.
+     */
     public static void main(String[] args) {
-        CCuenta miCuenta;
+        operativa_cuenta(2500f);
+    }
+
+    /**
+     * Realiza operaciones en una cuenta.
+     * @param cantidad Parámetro adicional (de tipo float) para futuras operaciones.
+     */
+    public static void operativa_cuenta(float cantidad) {
+        CCuenta cuenta1;
         double saldoActual;
 
-        miCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
-        saldoActual = miCuenta.estado();
-        System.out.println("El saldo actual es"+ saldoActual );
+        cuenta1 = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es " + saldoActual);
 
         try {
-            miCuenta.retirar(2300);
+            cuenta1.retirar(2300);
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
         }
         try {
             System.out.println("Ingreso en cuenta");
-            miCuenta.ingresar(695);
+            cuenta1.ingresar(695);
         } catch (Exception e) {
             System.out.print("Fallo al ingresar");
         }
